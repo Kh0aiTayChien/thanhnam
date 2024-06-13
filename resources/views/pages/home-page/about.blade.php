e2dd81744bcdc63defebab33e1ab34b41f677e94
Chiến Lê <81791505+Kh0aiTayChien@users.noreply.github.com>
2024-06-13 13:26:12 +0700

    Update about.blade.php

--- resources/views/pages/home-page/about.blade.php
+++ resources/views/pages/home-page/about.blade.php
@@ -6,11 +6,11 @@
     <div class=" row-test">
         <img src="{{asset('images/homepage/about/about.png')}}" class="col-lg-4 imgsec2-HP-test img-fluid border-radius" alt="" data-aos="fade-right" data-aos-duration="1500">
         <div class="col-lg-4 col-md-12 containercontentsec2-HP">
-            <p class="contentsec2-HP-test montserrat-semibold" data-aos="fade-left" data-aos-duration="1500"> Công ty TNHH kỹ thuật và thương mại Thành Nam thành lập vào tháng 11 năm 2019. Hoạt động
-                trong lĩnh vực xây dựng dân dụng, xây dựng giao thông hạ tầng và thi công nhà xưởng. Trong lĩnh vực xây
-                dựng công ty chúng tôi đã không ngừng cải tiến công nghệ đầu tư trang thiết bị thi công hiện đại, áp
-                dụng biện pháp thi công tiên tiến cùng với đội ngũ kỹ sư giỏi, công nhân lành nghề, công ty chúng tôi đã
-                thi công những công trình đạt tiến độ và chất lượng cao.</p><br>
+            <p class="contentsec2-HP-test montserrat-semibold" data-aos="fade-left" data-aos-duration="1500"> Công ty TNHH KT&TM Thành Nam (Thành Nam_S.Home) mang đến không gian sống tinh tế 
+                và hiện đại với đội ngũ Kiến trúc sư và Kỹ sư nhiều kinh nghiệm chúng tôi cam kết mang đến cho khách hàng những giải pháp thiết kế phù hợp với nhu cầu đáp ứng mọi nhu cầu của khách hàng. Bằng sự sáng tạo và chuyên nghiệp các công trình là những tác phẩm và không gian đáng sống.
+Thành Nam_ S.Home cung cấp dịch vụ thiết kế và thi công (Kiến trúc  - Nội thất) trọn gói với trách nhiệm cao nhất, ngoài ra chúng tôi cam kết về chủng loại vật liệu tốt nhất. Cùng với đó là chính sách bảo hành bảo trì khi có vấn đề sử dụng.
+Thành Nam_S.Home luôn đặt nhu cầu của khách hàng lên trên hết. Sự hài lòng của khách hàng là động lực để chúng tôi làm nên những không gian sống sang trọng và đẳng cấp.
+Chúng tôi không ngừng lỗ lực để mang đến dịch vụ tốt nhất cho khách hàng.</p><br>
             <div class="d-flex pb-5" data-aos="fade-left" data-aos-duration="1000">
                 <a href="/gioi-thieu"
                    class="btn blue-bg blue-text btn-block mb-3 text-white mitr-medium longer-btn shadow-test button-shake montserrat-bold">XEM THÊM</a>
@@ -28,11 +28,10 @@
             <img src="{{asset('images/homepage/about/about.png')}}" class="img-fluid border-radius" alt="">
         </div>
         <div class=" col-md-12 containercontentsec2-HP">
-            <p class="contentsec2-HP-test montserrat-semibold mt-4" style="font-size: 16px">Công ty TNHH kỹ thuật và thương mại Thành Nam thành lập vào tháng 11 năm 2019. Hoạt động
-                trong lĩnh vực xây dựng dân dụng, xây dựng giao thông hạ tầng và thi công nhà xưởng. Trong lĩnh vực xây
-                dựng công ty chúng tôi đã không ngừng cải tiến công nghệ đầu tư trang thiết bị thi công hiện đại, áp
-                dụng biện pháp thi công tiên tiến cùng với đội ngũ kỹ sư giỏi, công nhân lành nghề, công ty chúng tôi đã
-                thi công những công trình đạt tiến độ và chất lượng cao.</p><br>
+            <p class="contentsec2-HP-test montserrat-semibold mt-4" style="font-size: 16px">Công ty TNHH KT&TM Thành Nam (Thành Nam_S.Home) mang đến không gian sống tinh tế và hiện đại với đội ngũ Kiến trúc sư và Kỹ sư nhiều kinh nghiệm chúng tôi cam kết mang đến cho khách hàng những giải pháp thiết kế phù hợp với nhu cầu đáp ứng mọi nhu cầu của khách hàng. Bằng sự sáng tạo và chuyên nghiệp các công trình là những tác phẩm và không gian đáng sống.
+Thành Nam_ S.Home cung cấp dịch vụ thiết kế và thi công (Kiến trúc  - Nội thất) trọn gói với trách nhiệm cao nhất, ngoài ra chúng tôi cam kết về chủng loại vật liệu tốt nhất. Cùng với đó là chính sách bảo hành bảo trì khi có vấn đề sử dụng.
+Thành Nam_S.Home luôn đặt nhu cầu của khách hàng lên trên hết. Sự hài lòng của khách hàng là động lực để chúng tôi làm nên những không gian sống sang trọng và đẳng cấp.
+Chúng tôi không ngừng lỗ lực để mang đến dịch vụ tốt nhất cho khách hàng.</p><br>
             <div class="d-flex justify-content-center align-content-center">
                 <a href="/gioi-thieu"
                    class="btn blue-bg blue-text btn-block text-white mitr-medium longer-btn shadow-test button-shake montserrat-bold">XEM THÊM</a>
